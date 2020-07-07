442f15b8386a6dbf0d6d50cbfcd2987a01922709
Parth Mehta <work@iamdifferent.io>
2020-07-07 23:16:37 +1000

    Initial Commit

---
+++ Stackchat Web Messenger Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stackchat Web Messenger Demo
+//
+//  Created by Parth Mehta on 7/7/20.
+//  Copyright © 2020 Parth Mehta. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
